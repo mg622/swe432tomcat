a1f45c0c13793227fd4dfcdbed8bae0b88c015b3
mg622 <54865166+mg622@users.noreply.github.com>
2020-04-28 15:21:26 -0400

    Update assignment8.java

--- src/main/java/servlet/assignment8.java
+++ src/main/java/servlet/assignment8.java
@@ -212,7 +212,7 @@ static String censor(String text, String word)
     		out.println("<p>Store the data in XML format. </p>");
     		out.println("<p>Store the data into a database. ");
     		out.println("<p>Filter unacceptable words from the reviews (curse words). </p>");
-    		out.println("<p>Add use of a session object</p>");
+    		out.println("<p>Add use of a session object.</p>");
 		out.println("</p>");
 		out.println("<br>");
 		out.println("<p><b><center>Best Place to Study</center></b> </p>");
