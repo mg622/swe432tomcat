d75cd3cf097929983ee0a0b2a3b1643d2ccfa6d6
mg622 <54865166+mg622@users.noreply.github.com>
2020-05-09 23:03:56 -0400

    Update result.java

--- src/main/java/servlet/result.java
+++ src/main/java/servlet/result.java
@@ -22,7 +22,7 @@ public class result extends HttpServlet{
    protected void doPost  (HttpServletRequest req, HttpServletResponse res)
           throws ServletException, IOException{
      
-     response.setContentType("text/html");
+     res.setContentType("text/html");
       PrintWriter out = res.getWriter();
 
       out.println("<html>"); 
