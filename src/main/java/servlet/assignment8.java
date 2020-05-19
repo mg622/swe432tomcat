fa47fff54aec653853b893cce299ae6d82562c79
mg622 <54865166+mg622@users.noreply.github.com>
2020-05-19 19:43:05 -0400

    Update assignment8.java

--- src/main/java/servlet/assignment8.java
+++ src/main/java/servlet/assignment8.java
@@ -11,14 +11,14 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name = "finalExam", urlPatterns = { "/finalExam" })
+@WebServlet(name = "assignment8", urlPatterns = { "/assignment8" })
 
-public class finalExam extends HttpServlet {
+public class assignment8 extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	static String Domain = "";
 	static String Path = "/";
-	static String Servlet = "finalExam";
+	static String Servlet = "assignment8";
 	
 	static String OperationAdd = "Add";
 	static String OperationSub = "Subtract";
@@ -26,31 +26,34 @@ public class finalExam extends HttpServlet {
 
 	static String Style = "https://www.cs.gmu.edu/~offutt/classes/432/432-style.css";
 	
-	static enum Data {P1, FORM};
+	static enum Data {LOC, NOISE, CROWD, COMFORT};
   	static String RESOURCE_FILE = "entries.txt";
   	static final String VALUE_SEPARATOR = ";";
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String part1=request.getParameter(Data.P1.name());
-		String format = request.getParameter(Data.FORM.name());
-		
-		//String op=request.getParameter(Data.OP.name());
-		//String part2=request.getParameter(Data.P2.name());
-		
-		//String noise = request.getParameter(Data.NOISE.name());
-		//String crowd = request.getParameter(Data.CROWD.name());
-		//String comfort = request.getParameter(Data.COMFORT.name());
+		String location=request.getParameter(Data.LOC.name());
+		String noise = request.getParameter(Data.NOISE.name());
+		String crowd = request.getParameter(Data.CROWD.name());
+		String comfort = request.getParameter(Data.COMFORT.name());
      
      String error="";
-     if(part1==null){
-       error= "<li>Predicate is required</li>";
-       part1="";
+     if(location==null){
+       error= "<li>Location is required</li>";
+       location="";
      }
-     
-     if(format==null){
-    	 error+= "<li>Format is required.<li>";
-    	 format="";
+
+     if(noise==null){
+    	 error+= "<li>Rating for noise level is required.<li>";
+    	 noise="";
+     }
+     if(crowd == null){
+    	 error+= "<li>Rating for crowdedness is required.<li>";
+    	 crowd="";
+     }
+	if(comfort==null){
+    	 error+= "<li>Rating for comfort is required.<li>";
+    	 comfort="";
      }
 		
 
@@ -59,22 +62,15 @@ public class finalExam extends HttpServlet {
 
      if (error.length() == 0){
        PrintWriter entriesPrintWriter = new PrintWriter(new FileWriter(RESOURCE_FILE, true), true);
-       entriesPrintWriter.println(part1);
+       entriesPrintWriter.println(location+VALUE_SEPARATOR+noise+VALUE_SEPARATOR+crowd+VALUE_SEPARATOR+comfort);
        entriesPrintWriter.close();
 
        PrintHead(out);
-       if (format.equals("one"))
-       {
-       		PrintResponseBody(out, RESOURCE_FILE);
-       }
-       if (format.equals("1"))
-       {
-       		PrintResponseBody(out, RESOURCE_FILE);
-       }
+       PrintResponseBody(out, RESOURCE_FILE);
        PrintTail(out);
      }else{
        PrintHead(out);
-       PrintBody(out, part1, format, error);
+       PrintBody(out, location, noise, crowd, comfort, error);
        PrintTail(out);
      }
 		
@@ -103,13 +99,12 @@ static String censor(String text, String word)
   
     return result; 
 } 
-
 	
 
   private void PrintResponseBody (PrintWriter out, String resourcePath){
     out.println("<body onLoad=\"setFocus()\">");
     out.println("<p>");
-    out.println("Results Database");
+    out.println("Database");
     out.println("</p>");
     out.println("");
 	  
@@ -119,10 +114,10 @@ static String censor(String text, String word)
 	try {
 		out.println("  <table>");
         	out.println("  <tr>");
-        	//out.println("   <th></th>");
-        	//out.println("   <th>Noise</th>");
-			//out.println("   <th>Crowd</th>");
-			//out.println("   <th>Comfort</th>");
+        	out.println("   <th>Location</th>");
+        	out.println("   <th>Noise</th>");
+		out.println("   <th>Crowd</th>");
+		out.println("   <th>Comfort</th>");
         	out.println("  </tr>");
         	File file = new File(resourcePath);
         	if(!file.exists()){
@@ -137,77 +132,20 @@ static String censor(String text, String word)
         while ((line = bufferedReader.readLine()) != null) {
           String []  entry= line.split(VALUE_SEPARATOR);
           out.println("  <tr>");
-          
           for(String value: entry){ 
-          
 		//filter out curse words
-			String result=censor(value, "fuck");
-			result=censor(result, "Fuck");
-			result=censor(result, "Shit");
-			result=censor(result, "shit");
-		 	result=censor(result, "ass");
-		  	result=censor(result, "Ass");
-		  	result=censor(result, "bitch");
-		  	result=censor(result, "Bitch");
-		  	result=censor(result, "hell");
-		  	result=censor(result, "Hell");
+		String result=censor(value, "fuck");
+		result=censor(result, "Fuck");
+		result=censor(result, "Shit");
+		result=censor(result, "shit");
+		  result=censor(result, "ass");
+		  result=censor(result, "Ass");
+		  result=censor(result, "bitch");
+		  result=censor(result, "Bitch");
+		  result=censor(result, "hell");
+		  result=censor(result, "Hell");
 		  
-		  	
-		  
-		  	if (result.contains("and") || result.contains("And") || result.contains("AND") || result.contains("&")) 
-		  	{
-		
-		  		out.println("<br>");
-		  		out.println("<br>");
-		  		out.println(result.substring(0, 1));
-		  		out.println("&nbsp;");
-		  		out.println(result.substring(result.length() - 2));
-		  		out.println("&nbsp;&nbsp;&nbsp;");
-		  		out.println(result.substring(0, 1)+ " and " +result.substring(result.length() - 2));
-		  		
-		  		out.println("<br>");
-		  		out.println("<br>0  &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-		  		out.println("<br>0  &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-		  		out.println("<br>1  &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-		  		out.println("<br>1  &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  	}
-		  	
-		  	if (result.contains("xor") || result.contains("Xor") || result.contains("XOR") || result.contains("^")) 
-		  	{
-		  		out.println("<br>");
-		  		out.println("<br>");
-		  		out.println(result.substring(0, 1));
-		  		out.println("&nbsp;");
-		  		out.println(result.substring(result.length() - 2));
-		  		out.println("&nbsp;&nbsp;&nbsp;");
-		  		out.println(result.substring(0, 1)+ " xor " +result.substring(result.length() - 2));
-		  		
-		  		out.println("<br>0 &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-		  		out.println("<br>0 &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  		out.println("<br>1 &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  		out.println("<br>1 &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-
-		  	}
-		  	
-		  	else if (result.contains("or") || result.contains("Or") || result.contains("OR") || result.contains("|")) 
-		  	{
-		  		out.println("<br>");
-		  		out.println("<br>");
-		  		out.println(result.substring(0, 1));
-		  		out.println("&nbsp;");
-		  		out.println(result.substring(result.length() - 2));
-		  		out.println("&nbsp;&nbsp;&nbsp;");
-		  		out.println(result.substring(0, 1)+ " or " +result.substring(result.length() - 2));
-		  		
-		  		out.println("<br>0 &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 0<br>");
-		  		out.println("<br>0 &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  		out.println("<br>1 &nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  		out.println("<br>1 &nbsp; 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1<br>");
-		  	}
-		  	
-		  
-		  
-		  //out.println("   <td>"+result+"</td>");
+		  out.println("   <td>"+result+"</td>");
           }
           out.println("  </tr>");
         }
@@ -252,7 +190,7 @@ static String censor(String text, String word)
       		out.println("<p>Your session has been invalidated.</P>");
       	}
 		   
-        String lifeCycleURL = "/finalExam"; 
+        String lifeCycleURL = "/assignment8"; 
       	out.print  ("<br><a href=\"" + lifeCycleURL + "?action=invalidate\">");
 	out.println("<center>");
       	out.println("Invalidate the session</a>");
@@ -261,24 +199,24 @@ static String censor(String text, String word)
 	} 
 
 	
-	private void PrintBody (PrintWriter out, String part1, String format, String error){
+	private void PrintBody (PrintWriter out, String location, String noise, String crowd, String comfort, String error){
 	    
 		out.println("<body onLoad=\"setFocus()\">");
      		out.println("<p>");
-   		out.println("<b>Final Exam</b>");
+   		out.println("<b>Assignment 8 </b>");
 		out.println("<p>");
 		out.println("<b>Mei Gibbons</b>");
 		out.println("</p>");		
 		out.println("<p>");
-   		out.println("<b>OREs:</b>");
-    		out.println("<p>Input validation </p>");
-    		out.println("<p>Exclusive or ");
-    		out.println("<p>Multiple syntaxes in the input box for logical operators</p>");
-    		out.println("<p>Different formats to display the truth-values in the truth table</p>");
+   		out.println("<b>Additional features:</b>");
+    		out.println("<p>Store the data in XML format. </p>");
+    		out.println("<p>Store the data into a database. ");
+    		out.println("<p>Filter unacceptable words from the reviews (curse words). </p>");
+    		out.println("<p>Add use of a session object.</p>");
 		out.println("</p>");
 		out.println("<br>");
-		out.println("<p><b><center>Final Exam</center></b> </p>");
-		out.println("<p><center></center></p>");
+		out.println("<p><b><center>Best Place to Study</center></b> </p>");
+		out.println("<p><center>Fill out the form to rate study areas on campus!</center></p>");
      		out.println("</p>");
 
      		if(error != null && error.length() > 0){
@@ -293,8 +231,8 @@ static String censor(String text, String word)
 		out.println("<center>");
 		out.println(" <table>");
      		out.println("  <tr>");
-     		out.println("   <td>Predicate (Ex: A and B, A && B, etc.):</td>");
-     		out.println("   <td><input type=\"text\" name=\""+Data.P1.name()+"\" value=\""+part1+"\" size=30 required></td>");
+     		out.println("   <td>Location:</td>");
+     		out.println("   <td><input type=\"text\" name=\""+Data.LOC.name()+"\" value=\""+location+"\" size=30 required></td>");
      		out.println("  </tr>");
      		out.println("  <tr>");
 		out.println(" </table>");
@@ -302,16 +240,21 @@ static String censor(String text, String word)
 		out.println("<br>");
 		
 		out.println("<center>");
-		out.println("<b>Choose the format:</b>");
+		out.println("<b>Rating for Noise Level:</b> (1-very loud, 5-silent)");
 		out.println("<br>");
-		out.println("  <input type=\"radio\" name=\""+Data.FORM.name() +"\" id=\"one\" value=\"1\">");
-		out.println("  <label for=\"one\">1-0&nbsp;&nbsp;</label>"); 
-		
-		out.println("  <input type=\"radio\"name=\""+Data.FORM.name() +"\" id=\"two\" value=\"2\" />");
-		out.println("  <label for=\"two\">t-f</label>");
+		out.println("  <input type=\"radio\" name=\""+Data.NOISE.name() +"\" id=\"one\" value=\"1\">");
+		out.println("  <label for=\"one\">1</label>"); 
+		out.println("  <input type=\"radio\"name=\""+Data.NOISE.name() +"\" id=\"two\" value=\"2\" />");
+		out.println("  <label for=\"two\">2</label>");
+		out.println("  <input type=\"radio\" name=\""+Data.NOISE.name() +"\" id=\"three\" value=\"3\" checked=\"true\" />");
+		out.println("  <label for=\"three\">3</label>");
+		out.println("  <input type=\"radio\" name=\""+Data.NOISE.name() +"\" id=\"four\" value=\"4\" />");
+		out.println("  <label for=\"four\">4</label>");
+		out.println("  <input type=\"radio\" name=\""+Data.NOISE.name() +"\" id=\"five\" value=\"5\" />");
+		out.println("  <label for=\"five\">5</label>");
 		out.println("<p></p>");
 
-		/*out.println("<br> ");
+		out.println("<br> ");
 		out.println("<b>Rating for Crowdedness:</b> (1-way too crowded, 5-hardly any people)");    
 		out.println("<br>");
 		out.println("  <input type=\"radio\" name=\""+Data.CROWD.name() +"\" id=\"one\" value=\"1\">");
@@ -339,9 +282,8 @@ static String censor(String text, String word)
 		out.println("  <label for=\"four\">4</label>");
 		out.println("  <input type=\"radio\" name=\""+Data.COMFORT.name() +"\" id=\"five\" value=\"5\" />");
 		out.println("  <label for=\"five\">5</label>");
-		out.println("<p></p>");*/
+		out.println("<p></p>");
 		
-		out.println("<center>");
 		out.println("<input type=\"submit\" onclick=\"doPost()\" value=\"Submit\">");
 
      		out.println("");
@@ -357,7 +299,7 @@ static String censor(String text, String word)
 		out.println("<html>");
      		out.println("");
      		out.println("<head>");
-     		out.println("<title>Final Exam</title>");
+     		out.println("<title>Assignment 8</title>");
      		out.println ("<script>");
      		out.println ("  function setFocus(){");
      		out.println ("    document.persist2file.NAME.focus();");
@@ -369,7 +311,7 @@ static String censor(String text, String word)
 	
 	
 	private void PrintBody(PrintWriter out) {
-		PrintBody(out, "", null, null);
+		PrintBody(out, "", null, null, null, null);
 	}
 
 	
